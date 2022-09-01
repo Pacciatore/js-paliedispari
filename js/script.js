408fb9ab8e295de375211b15c5f4001a38053822
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-01 10:27:25 +0200

    Pari e Dispari

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 console.log('JS OK');
 
-const word = prompt('Inserire una parola:');
+let userNumber = 0;
+let userChoice = '';
+let sumResultOdd = '';
 
-const isPalindroma = checkPalindroma(word);
+// const word = prompt('Inserire una parola:');
 
-
-if (isPalindroma) {
-    alert('La parola è palindroma!');
-} else {
-    alert('La parola non è palindroma!');
-}
+// const isPalindroma = checkPalindroma(word);
 
 
+// if (isPalindroma) {
+//     alert('La parola è palindroma!');
+// } else {
+//     alert('La parola non è palindroma!');
+// }
 
+do {
+    userChoice = prompt('pari o dispari?')
+} while (userChoice.charAt(0) != 'p' && userChoice.charAt(0) != 'd');
 
+console.log(userChoice);
 
+do {
+    userNumber = parseInt(prompt('Inserire un numero da 1 a 5:'));
+    console.log('Numero dell\'utente: ' + userNumber);
+} while (userNumber < 1 || userNumber > 5);
+
+const pcNumber = pcRandomNumber();
+console.log('Numero del computer: ' + pcNumber);
+
+const sum = userNumber + pcNumber;
+
+const isOdd = checkOdd(sum);
+
+isOdd === true ? sumResultOdd = 'd' : sumResultOdd = 'p';
+
+alert(sumResultOdd === userChoice.charAt(0) ? 'Hai Vinto!' : 'Hai Perso!');
 
 function checkPalindroma(text) {
     console.log(text);
@@ -32,4 +53,15 @@ function checkPalindroma(text) {
 
     const isEqual = reversedText === text;
     return isEqual;
+}
+
+function pcRandomNumber() {
+    const pcNumber = Math.floor(Math.random() * 5) + 1;
+    return pcNumber;
+}
+
+function checkOdd(number) {
+    const isOdd = (number % 2) === 1;
+    console.log('è dispari? ' + isOdd);
+    return isOdd;
 }
