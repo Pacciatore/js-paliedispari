67577e607614a708589b4e6836709c2d480e81b7
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-01 09:53:15 +0200

    Palindrome check

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 console.log('JS OK');
+
+const word = prompt('Inserire una parola:');
+
+const isPalindroma = checkPalindroma(word);
+
+
+if (isPalindroma) {
+    alert('La parola è palindroma!');
+} else {
+    alert('La parola non è palindroma!');
+}
+
+
+
+
+
+
+function checkPalindroma(text) {
+    console.log(text);
+
+    let reversedText = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text.charAt(i);
+
+        reversedText = char + reversedText;
+
+    }
+
+    console.log(reversedText);
+
+    const isEqual = reversedText === text;
+    return isEqual;
+}
